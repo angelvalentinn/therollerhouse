1abb05e71d5c0c19a98b55ace893d6d72d9df592
angelvalentinn <altieriange1@gmail.com>
2025-07-25 18:04:34 -0300

    add imgs products and update detail

--- src/scripts/productos.js
+++ src/scripts/productos.js
@@ -3,31 +3,77 @@ export const PRODUCTOS = [
         id: 1, 
         title: "Blackout Premium", 
         description: [
-            "En The Roller House, nuestras cortinas blackout están diseñadas para bloquear hasta el 99% de la luz exterior, creando un ambiente oscuro y relajante ideal para dormir, ver películas o regular la temperatura en cualquier espacio",
-            // "Ideal para: Dormitorios, home theaters, guarderías y espacios donde la privacidad y el confort son esenciales.",
-            // "Ventajas: Soldable, Apto para impresiones, rotable"
+            "Diseñadas para bloquear 99% de la luz exterior, creando un ambiente oscuro y relajante ideal para dormir y ver películas",
         ],
-        especifications: [
-            ["Composición:","30% Fibra de vidrio | 70% PVC"],
-            ["Peso por M2:","530 G/M2"],
-            ["Capas:","4"],
-            ["Ancho disponible:","183/2.50"],
-            ["Longitud de rollo:", "30 MTS"]
-        ],
-        colors: [
-            ["Blanco", "/imgs/colores/blackout-premium/blanco.jpeg"],
-            ["Gris", "/imgs/colores/blackout-premium/gris.jpeg"],
-            ["Negro", "/imgs/colores/blackout-premium/negro.jpeg"],
-            ["Beige", "/imgs/colores/blackout-premium/beige.jpeg"]
-        ]
+        ideal: ["Dormitorios, home theaters, guarderías o turnos de noche"],
+        colors: ["Blanco | Natural | Gris | Beige | Negro"],
+        gamaColors: "/imgs/colores/blackout-premium.jpeg",
+        imgs: ["/imgs/telas/blackout-premium.png"]
+
     },
 
-    {id: 2, title: "Screen 5% Nordic", description: ["Diseñadas para ofrecer control de luz y privacidad con un estilo minimalista. Disponibles en tejidos translúcidos, opacos o blackout, se adaptan a cualquier espacio.","Ideal para: Oficinas, hogares, vidrieras","Beneficios: Limpieza sencilla, protección solar, elegancia discreta (sin obstruir la vista exterior)"]},
-    {id: 3, title: "Screen Poliéster 3% Nordic", description: ["Controla la luz natural con elegancia: filtra los rayos solares sin oscurecer completamente el ambiente. Tejido técnico que difumina la luminosidad, reduciendo el resplandor y protegiendo tus muebles de los rayos UV, manteniendo una atmósfera acogedora y con privacidad.","Ideal para: Salas de estar, cocinas, oficinas.", "Beneficios clave: Regula la temperatura, estética moderna, luz suave."]},
-    {id: 4, title: "Screen 5% Solar Cool", description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."]},
-    {id: 5, title: "Screen 5% Poli", description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."]},
-    {id: 6, title: "Punteado Gaspeado", description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."]},
-    {id: 7, title: "Texturado Lino 7%", description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."]},
-    {id: 8, title: "Sun Screen Gaspeado", description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."]}, 
-    {id: 9, title: "Sun Screen Premium texturado", description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."]}, 
+    {   
+        id: 2, 
+        title: "Screen 5% Nordic",
+        description: [`Diseñadas para permitir que la luz se filtre hacia adentro y para "ver a través" cuando se bajan las persianas.
+        Estas cortinas son conocidas por su capacidad de transformar la decoración de cualquier habitacion y al mismo tiempo permitir la sensación del mundo exterior en su hogar.`],
+        ideal: ["Salas, oficinas o espacios donde buscas luz natural sin deslumbramientos"],
+        colors: ["Blanco | Blanco Gris | Blanco Beige | Negro | Chocolate"],
+        imgs: ["/imgs/telas/screen5nordic.png"]
+    },
+
+    {
+        id: 3,
+        title: "Screen Poliéster 3% Nordic",
+        description: ["Confeccionada en tejido de poliéster con apertura del 3%, permite paso controlado de luz natural mientras preserva la privacidad y protege contra los rayos UV. "],
+        ideal: ["Oficinas con pantalla, Salas de estar luminosas, Cocinas y dormitorios"],
+        colors: ["Blanco | Blanco Gris | Blanco Beige"],
+        imgs: ["/imgs/telas/screen3poliester-nordic.png"]
+    },
+
+    {   id: 4,
+        title: "Screen 5% Solar Cool",
+        description: ["Ofrece un equilibrio ideal entre visibilidad exterior y protección solar. Con una apertura del 5%, permite el paso de luz natural sin perder privacidad, reduciendo el deslumbramiento y el calor en ambientes interiores."],
+        ideal: ["Livings, Oficinas modernas, Comedores y espacios con buena luz natural"],
+        colors: ["Blanco | Blanco Beige | Blanco Gris | Beige | Gris"],
+        imgs: ["/imgs/telas/screen5solarcool.png"]
+    },
+
+    {   id: 5,
+        title: "Screen 5% Poli",
+        description: ["Brinda una iluminación natural suave y permite ver hacia el exterior gracias a su apertura del 5%. Fabricada en tejido de poliéster resistente, ofrece protección solar, privacidad y confort visual."],
+        ideal: ["Oficinas, livings, Cocinas o espacios de trabajo donde se desea luz natural sin deslumbramiento"],
+        colors: ["Blanco | Negro | Gris | Beige | Blanco Beige | Blanco Gris | Gris Negro"],
+        imgs: ["/imgs/telas/screen5poli.png"]
+    },
+
+    {   id: 6,
+        title: "Punteado Gaspeado",
+        description: ["Cortina roller fabricada en tela punteado gaspeado, un tejido semitransparente con textura de puntos que filtra la luz creando un ambiente luminoso y con privacidad."],
+        ideal: ["Salas y comedores, Oficinas y locales comerciales modernos, Dormitorios, Espacios de día"],
+        colors: ["Lino Punteado 5% Perla y Lino | Screen Gaspeado Lino 5% | Gris Beige | Chocolate"],
+        imgs: ["/imgs/telas/punteado-gaspeado.png"]
+    },
+
+    {   id: 7, 
+        title: "Texturado Lino 7%", 
+        description: ["Cortina roller en tejido tipo lino texturizado con 7% de apertura. Ofrece un equilibrio perfecto entre luz natural y privacidad, con un aspecto rústico-moderno."],
+        ideal: ["Casas de campo o decoración nórdica, Cocinas y baños, Espacios de Trabajo modernos"],
+        colors: ["Blanco | Blanco Beige | Blanco Gris | Negro"],
+        imgs: ["/imgs/telas/texturadolino7.png"]
+    },
+
+    {   id: 8, 
+        title: "Sun Screen Gaspeado Premium 5%", 
+        description: ["Cortina roller de alta gama en tela Sun Screen con tratamiento gaspeado premium (5% de apertura). Ofrece máxima protección solar manteniendo visibilidad hacia el exterior."],
+        ideal: ["Ventanales grandes, Oficinas modernas, Climas Cálidos"],
+        colors: ["Blanco Gris | Gris Negro"]
+    },
+
+    {   id: 9, 
+        title: "Sun Screen Premium texturado", 
+        description: ["Combina protección solar eficiente con un acabado texturado de alta gama que aporta elegancia y estilo. Su tejido técnico permite el paso controlado de luz, mantiene la vista al exterior y reduce el calor y el deslumbramiento"],
+        ideal: ["Salas de estar, Oficinas modernas, livings, espacios comerciales"],
+        colors: ["Blanco | Blanco Beige | Negro Gris"]
+    }, 
 ]
